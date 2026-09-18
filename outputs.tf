9f3718ff6aae91f6fecb2d74a0b87347e50f46a8
Ricardo A <rjaraujo23@gmail.com>
2026-09-18 12:10:17 +0200

    feat: export instance_id output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "IP publica asignada a la instancia EC2"
   value       = aws_instance.web_server.public_ip
 }
+
+otput "instance_id" {
+  description = "ID de la instancia EC2"
+  value       = aws_instance.web_server.id
+}
